d15a961c6f2aa7017a9a1b5d73647d683f8f5e79
Lenon <lenonphilippi@gmail.com>
2023-07-10 22:13:22 -0300

    [lenon fp] Projeto Trybers and Dragons

---
+++ src/Races/Races.ts
@@ -0,0 +1,9 @@
+export default class Races {
+  private name: string;
+  private dexterity: number;
+
+  constructor(name: string, dex: number) {
+    this.name = name;
+    this.dexterity = dex;
+  }
+}
