3f5c4911f0350fe9842b42e597a872fb067e8414
Lenon <lenonphilippi@gmail.com>
2023-07-16 23:09:28 -0300

    requisito 7 ok

--- src/Archetypes/index.ts
+++ src/Archetypes/index.ts
@@ -1,8 +1,8 @@
-import Archetypes from './Archetype';
+import Archetype from './Archetype';
 import Mage from './Mage';
 import Warrior from './Warrior';
 import Ranger from './Ranger';
 import Necromancer from './Necromancer';
 
 export { Mage, Warrior, Ranger, Necromancer };
-export default Archetypes;
+export default Archetype;
